fd90c1998d1aa0eccff788245ef58e460cd5d307
Carlos Heredia <carlos.valenzh@gmail.com>
2022-02-13 17:58:03 -0600

    Avance hasta clase 21

--- intro-python/index.py
+++ intro-python/index.py
@@ -1,2 +1,43 @@
-if 3>5:
-	print('Esto no se va a imprimir')
+#Acá van los comentarios
+#if 3>5:
+#print('Esto no se va a imprimir')
+#if 5>3:
+#        print('5 es mayor que 3')
+x=5
+y='Chanchito feliz'
+
+#print(x,y)
+
+email= 'carlos.valenzh@gmail.com'
+#print(email)
+
+a, b, c = 'Lala','lele','lili'
+
+#print(a,b,c)
+
+valor1=valor2=valor3= 'chanchito feliz'
+
+#print(valor1,valor2,valor3)
+
+inicio='Hola '
+final= 'Mundo'
+
+#print (inicio + final)
+
+palabra='Hola mundo'
+palabra1="Hola mundo"
+num_entero= 20
+num_decimales=20.2
+num_complejo=1j
+
+#print(palabra,palabra1,num_entero,num_decimales,num_complejo)
+
+lista=[1,2,3]
+lista1 =lista.copy()
+lista.append(4)
+lista.append(3)
+#lista.clear()
+#print(lista,len(lista))
+largo_lista=len(lista)
+largo_lista1=len(lista1)
+print(largo_lista,largo_lista1)
